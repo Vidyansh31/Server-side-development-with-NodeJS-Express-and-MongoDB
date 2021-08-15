efd018df6a695e0bad324870d20e274f412b291a
Vidyansh31 <deva5084@gmail.com>
2021-08-15 13:54:22 +0530

    Serving static Html files using MORGAN

--- node-express/index.js
+++ node-express/index.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const http = require('http');
+const morgan = require('morgan');
 
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express();  // here we are saying that our application are going to use express node module
+
+app.use(morgan('dev')); //it will print out additional information to the screen as required
+
+app.use(express.static(__dirname + '/public')); // This will serve the static files
 
 app.use((req, res, next) => {
     console.log(req.headers);
