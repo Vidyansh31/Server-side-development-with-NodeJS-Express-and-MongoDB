f1a033c4e1c2b584f78d672975813d0adb268c09
Vidyansh31 <deva5084@gmail.com>
2021-08-28 21:53:37 +0530

    Express-Session II

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -17,13 +17,13 @@ const Dishes = require('./models/dishes');
 const Promotions = require('./models/promotion');
 const leaders = require('./models/leader');
 
-const url ='mongodb://localhost:27017/confusion';
+const url = 'mongodb://localhost:27017/confusion';
 
 const connect = mongoose.connect(url);
 
-connect.then((db)=>{
+connect.then((db) => {
   console.log('Connected Correctly to server');
-},(err)=>{console.log(err);});
+}, (err) => { console.log(err); });
 
 var app = express();
 
@@ -40,80 +40,49 @@ app.use(express.urlencoded({ extended: false }));
 app.use(session({
   name: 'session-id',
   secret: '12345-67890-09876-54321',
-  saveUninitialized:false,
+  saveUninitialized: false,
   resave: false,
-  store : new FileStore()
+  store: new FileStore()
 }));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
 function auth(req, res, next) {
   console.log(req.session);
   // check if the session is set or not
-  if(!req.session.user)
-  {
-    //If not Set then we need to get authentication
-    var authHeader = req.headers.authorization;
+  if (!req.session.user) {
+    var err = new Error('You are not authenticated');
+    err.statuscode = 401;
+    return next(err);
 
-    if(!authHeader) {
-        var err = new Error('You are not authenticated');
-    
-        res.setHeader('WWW-Authenticate','Basic');
-        err.statuscode = 401;
-        return next(err);
-        
-      }
-      var auth = new Buffer.from(authHeader.split(' ')[1],'base64').toString().split(':');
-      var username = auth[0];
-      var password = auth[1];
-    
-      if(username==="admin" && password==="password")
-      {
-        //Creating a Session
-        req.session.user = "admin";
-        next();//authorized
-      }
-      else
-      {
-        var err = new Error('You are not authorized');
-    
-        res.setHeader('WWW-Authenticate', 'Basic');
-        err.status = 401;
-        return next(err);
-      }
-     
   }
-  else{
-    if(req.session.user === 'admin')
-    {
-      console.log('req.session: ',req.session);
-      next();
-    }
-    else{
-      var err = new Error('You are not authorized');
-    
-       
-        err.status = 401;
-        return next(err);
-    }
+  else if (req.session.user === 'authenticated') {
+    next();//authorized
   }
-  
-}
+  else {
+    var err = new Error('You are not authorized');
+    err.status = 401;
+    return next(err);
+  }
+
+};
 
 app.use(auth);
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/dishes',dishRouter);
-app.use('/promotions',promoRouter);
-app.use('/leaders',leaderRouter);
+
+app.use('/dishes', dishRouter);
+app.use('/promotions', promoRouter);
+app.use('/leaders', leaderRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
